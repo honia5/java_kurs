5d8a41d8169395184c883b64c84963fb56b52d85
robert <robert.honca@gmail.com>
2017-07-27 09:32:50 +0200

    added some functions to my first program

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,27 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("User");
+    hello("Robert");
+
+    double len = 5;
+    System.out.println("Pole kwadratu o boku " + len + " = " + (area(len)));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Pole prostokata o bokach " + a + " i " + b +" = " + (area(a, b)));
+
+  }
+
+  public static void hello(String somebody) {
+
+    System.out.println("Hello," + somebody + "!");
+  }
+  public static double area(double l){
+    return l * l;
+  }
+  public static double area(double a, double b){
+    return a * b;
   }
 }

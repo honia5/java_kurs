30ad4a7296a6d7db19c4ed13ffb66e1691aba743
robert <robert.honca@gmail.com>
2017-08-18 11:25:38 +0200

    Creating a new contact with the refactor of code for
    Exercise 4

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactDate.java
@@ -0,0 +1,43 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactDate {
+  private final String firrstname;
+  private final String lastname;
+  private final String address;
+  private final String mobile;
+  private final String email;
+  private final String home;
+
+  public ContactDate(String firrstname, String lastname, String address, String mobile, String email, String home) {
+    this.firrstname = firrstname;
+    this.lastname = lastname;
+    this.address = address;
+    this.mobile = mobile;
+    this.email = email;
+    this.home = home;
+  }
+
+  public String getFirrstname() {
+    return firrstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getHome() {
+    return home;
+  }
+}

06c7a77842753f707fec56341df13c8b85638bc4
robert <robert.honca@gmail.com>
2017-09-21 22:04:55 +0200

    Optimizing forms and method type

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -8,7 +8,7 @@ public class GroupCreationTest extends TestBase {
   public void GroupCreationTest() {
     app.getNavigateHelper().gotoGroupPage();
     app.getGroupHelper().initGroupCration();
-    app.getGroupHelper().fillGroupForm(new GroupDate("test1", "test2", "test3"));
+    app.getGroupHelper().fillGroupForm(new GroupDate("test1", null, null));
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupPage();
   }

25ae135171f4754e0c0c6a5df29ef73f9f450573
robert <robert.honca@gmail.com>
2017-10-02 21:01:16 +0200

    Array as List
    Use array as lis and always use the type the parameter in the list example: <String>

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.sandbox;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -11,13 +12,9 @@ public class Collections {
   public static void main(String [] args){
     String[] langs = {"Java", "C#","Python","PHP"};
 
-    List<String> languages =  new ArrayList<String>();
-    languages.add("Java");
-    languages.add("C#");
-    languages.add("Python");
-    languages.add("PHP");
-
-    for (String l : langs){
+    List<String> languages =  Arrays.asList("Java", "C#","Python","PHP");
+    
+    for (String l : languages){
       System.out.println("Chcę sie nauczyć " + l);
     }
 

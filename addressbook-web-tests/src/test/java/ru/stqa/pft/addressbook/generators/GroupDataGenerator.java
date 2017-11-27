f2fd674725bea99d11f6e2d4f2fec2a402206e1a
robert <robert.honca@gmail.com>
2017-11-28 00:20:14 +0100

    6.3 Jcommander
    3. Analiza opcji wiersza poleceń przy uruchomieniu generatora danych
    Bibioteka Jcommader okresla ona list atrzbow klasie

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.GroupDate;
 
 import java.io.File;
@@ -14,15 +17,35 @@ import java.util.List;
  */
 public class GroupDataGenerator {
 
-  public static void main(String[] args) throws IOException {
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
+  @Parameter(names = "-c", description = "Group count")
+  public int count;
 
-    List<GroupDate> groups = generateGroups(count);
-    save(groups, file);
+  @Parameter(names = "-f", description = "Target file")
+
+  public String file;
+
+  public static void main(String[] args) throws IOException {
+    GroupDataGenerator generator = new GroupDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    }catch (ParameterException ex){
+      jCommander.usage();
+      return;
+    }
+
+    generator.run();
+    
+
   }
-//save file
-  private static void save(List<GroupDate> groups, File file) throws IOException {
+
+  private void run() throws IOException {
+    List<GroupDate> groups = generateGroups(count);
+    save(groups, new File(file));
+  }
+
+  //save file
+  private void save(List<GroupDate> groups, File file) throws IOException {
     System.out.println(new File(".").getAbsolutePath());
     Writer writer = new FileWriter(file);
     for(GroupDate group : groups){
@@ -31,7 +54,7 @@ public class GroupDataGenerator {
     writer.close();
   }
 //genereate file
-  private static List<GroupDate> generateGroups(int count) {
+  private List<GroupDate> generateGroups(int count) {
     List<GroupDate> groups = new ArrayList<GroupDate>();
     for(int i = 0; i < count; i++){
       groups.add(new GroupDate().withName(String.format("test %s", i))

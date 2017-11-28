ff110fdbd27f82d374b31f27ed56b76ed238e146
robert <robert.honca@gmail.com>
2017-11-28 22:12:14 +0100

    6.5 Separate data from test and add it from the file
    Oddzielenie danych od testu i ładowanie z pliku

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -1,10 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.apache.commons.lang3.ObjectUtils;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupDate;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import java.io.*;
+import java.nio.Buffer;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -15,11 +18,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTest extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validGroups() {
+  public Iterator<Object[]> validGroups() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[] {new GroupDate().withName("test1").withHeader("header 1").withFooter("footer 1")});
-    list.add(new Object[] {new GroupDate().withName("test2").withHeader("header 2").withFooter("footer 2")});
-    list.add(new Object[] {new GroupDate().withName("test3").withHeader("header 3").withFooter("footer 3")});
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    String line = reader.readLine();
+    while (line != null){
+      String[] split = line.split(";");
+      list.add(new Object[]{new GroupDate().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 

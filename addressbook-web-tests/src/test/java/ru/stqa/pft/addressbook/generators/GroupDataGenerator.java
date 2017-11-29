e2c14a6424f722500c606c6008eb0c6d27604676
robert <robert.honca@gmail.com>
2017-11-29 02:04:18 +0100

    6.6 Format XML
    zastosowanie możliwośći bibioteki Xstream

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.GroupDate;
 
 import java.io.File;
@@ -21,8 +22,10 @@ public class GroupDataGenerator {
   public int count;
 
   @Parameter(names = "-f", description = "Target file")
-
   public String file;
+
+  @Parameter(names = "-d", description = "Data format")
+  public String format;
 
   public static void main(String[] args) throws IOException {
     GroupDataGenerator generator = new GroupDataGenerator();
@@ -41,11 +44,27 @@ public class GroupDataGenerator {
 
   private void run() throws IOException {
     List<GroupDate> groups = generateGroups(count);
-    save(groups, new File(file));
+    if (format.equals("csv")){
+      saveAsCsv(groups, new File(file));
+    } else if (format.equals("xml")) {
+      saveAsXml(groups, new File(file));
+    } else {
+      System.out.println("Unrecognized format" + format);
+    }
+  }
+
+  private void saveAsXml(List<GroupDate> groups, File file) throws IOException {
+    XStream xstream = new XStream();
+    xstream.processAnnotations(GroupDate.class);
+    String xml = xstream.toXML(groups);
+    Writer writer = new FileWriter(file);
+    writer.write(xml);
+    writer.close();
+
   }
 
   //save file
-  private void save(List<GroupDate> groups, File file) throws IOException {
+  private void saveAsCsv(List<GroupDate> groups, File file) throws IOException {
     System.out.println(new File(".").getAbsolutePath());
     Writer writer = new FileWriter(file);
     for(GroupDate group : groups){
@@ -58,7 +77,7 @@ public class GroupDataGenerator {
     List<GroupDate> groups = new ArrayList<GroupDate>();
     for(int i = 0; i < count; i++){
       groups.add(new GroupDate().withName(String.format("test %s", i))
-              .withHeader(String.format("header %s", i)).withFooter(String.format("footer %s", i)));
+              .withHeader(String.format("header\n%s", i)).withFooter(String.format("footer\n%s", i)));
     }
     return groups;
   }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.apache.commons.lang3.ObjectUtils;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -11,6 +12,8 @@ import java.nio.Buffer;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -19,15 +22,17 @@ public class GroupCreationTest extends TestBase {
 
   @DataProvider
   public Iterator<Object[]> validGroups() throws IOException {
-    List<Object[]> list = new ArrayList<Object[]>();
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+    String xml = "";
     String line = reader.readLine();
     while (line != null){
-      String[] split = line.split(";");
-      list.add(new Object[]{new GroupDate().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      xml += line;
       line = reader.readLine();
     }
-    return list.iterator();
+    XStream xStream = new XStream();
+    xStream.processAnnotations(GroupDate.class);
+    List<GroupDate> goups = (List<GroupDate>) xStream.fromXML(xml);
+    return goups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
   }
 
   @Test(dataProvider = "validGroups")
